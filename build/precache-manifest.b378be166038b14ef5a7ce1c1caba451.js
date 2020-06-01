a05fc4b88cf90e98afb3a6dc01e9c2039d6aa7d7
mdabdullahbaig <abdullahhussainbaig1997@gmail.com>
2020-06-01 17:20:33 +0530

    add build folder

---
+++ build/precache-manifest.b378be166038b14ef5a7ce1c1caba451.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6392a2a48e9590a88167b90cdffaf9ab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b315113b025b2b0a9744",
+    "url": "/static/css/2.7c6836da.chunk.css"
+  },
+  {
+    "revision": "9d0748ff8bf951363623",
+    "url": "/static/css/main.59f3f085.chunk.css"
+  },
+  {
+    "revision": "b315113b025b2b0a9744",
+    "url": "/static/js/2.f779b4cc.chunk.js"
+  },
+  {
+    "revision": "5966da8079260337a8884c83a346e83c",
+    "url": "/static/js/2.f779b4cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d0748ff8bf951363623",
+    "url": "/static/js/main.e8728fb7.chunk.js"
+  },
+  {
+    "revision": "b4ba650b238755144e2a",
+    "url": "/static/js/runtime-main.e5fc83a6.js"
+  }
+]);
